1537edb0e2f17239d3c819bb800a4cf679d843ca
masaya.ishioka <masaya.ishioka@pa-rk.co.jp>
2019-07-04 17:20:43 +0900

    routingに追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,21 @@ import { ProductListComponent } from './product/product-list/product-list.compon
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 import { ProductEditComponent } from './product/product-edit/product-edit.component';
 import { ProductNewComponent } from './product/product-new/product-new.component';
+import { HeroFightComponent } from './heros/hero-fight/hero-fight.component';
+import { HeroIndexComponent } from './heros/hero-index/hero-index.component';
+import { HeroGetComponent } from './heros/hero-get/hero-get.component';
+import { HeroSetComponent } from './heros/hero-set/hero-set.component';
+
 const routes: Routes = [
 	{ path: 'products', component: ProductListComponent},
 	{ path: 'products/new', component: ProductNewComponent },
 	{ path: 'products/:id', component: ProductDetailComponent },
 	{ path: 'products/:id/edit', component: ProductEditComponent },
 	{ path: '', redirectTo: '/products', pathMatch: 'prefix'  },
+	{ path: 'heros', component: HeroIndexComponent },
+	{ path: 'heros/set', component: HeroSetComponent },
+	{ path: 'heros/get', component: HeroGetComponent },
+	{ path: 'heros/fight', component: HeroFightComponent },
 ];
 
 @NgModule({
